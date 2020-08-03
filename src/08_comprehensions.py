720a13ef8ed16a665a6edb8908dba9f144c9e980
Marta Krawczyk <martaalicja113@gmail.com>
2020-08-03 19:44:56 -0400

    pushing changes

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -10,14 +10,18 @@ for more info regarding list comprehensions.
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = []
+y = [x for x in range(1,6)]
+# for i in range(1, 6):
+#     y.append(i)
 
-print (y)
+print(y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = []
+y = [x**3 for x in range(10)]
+# for i in range(10):
+#     y.append(i**3)
 
 print(y)
 
@@ -26,7 +30,9 @@ print(y)
 
 a = ["foo", "bar", "baz"]
 
-y = []
+y = [each.upper() for each in a]
+# for each in a:
+#     y.append(each.upper())
 
 print(y)
 
@@ -36,6 +42,7 @@ print(y)
 x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
-y = []
+y = [num for num in x if int(num) % 2 == 0]
 
 print(y)
+# print(type(x[0]))
--- src/11_args.py
+++ src/11_args.py
@@ -5,6 +5,9 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(num1, num2):
+    return num1+num2
+
 
 print(f1(1, 2))
 
@@ -14,6 +17,11 @@ print(f1(1, 2))
 
 # YOUR CODE HERE
 
+
+def f2(*integers):
+    return sum(integers)
+
+
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
@@ -22,6 +30,12 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
+
+
+def f2(array):
+    return sum(array)
+
+
 print(f2(a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
@@ -30,6 +44,11 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(*args):
+    if len(args) == 1:
+        return args[0] + 1
+    else:
+        return sum(args)
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -44,6 +63,8 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**args):
+   print('key',args)
 
 # Should print
 # key: a, value: 12
@@ -54,12 +75,12 @@ f4(a=12, b=30)
 # key: city, value: Berkeley
 # key: population, value: 121240
 # key: founded, value: "March 23, 1868"
-f4(city="Berkeley", population=121240, founded="March 23, 1868")
+# f4(city="Berkeley", population=121240, founded="March 23, 1868")
 
-d = {
-    "monster": "goblin",
-    "hp": 3
-}
+# d = {
+#     "monster": "goblin",
+#     "hp": 3
+# }
 
-# How do you have to modify the f4 call below to make this work?
-f4(d)
+# # How do you have to modify the f4 call below to make this work?
+# f4(d)
