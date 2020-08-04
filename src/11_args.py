c7baca41c037c7bb4e02165c43eaef073f068536
Marta Krawczyk <martaalicja113@gmail.com>
2020-08-04 17:49:48 -0400

    pushing changes for day 2

--- src/11_args.py
+++ src/11_args.py
@@ -31,12 +31,11 @@ a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
 
+# def f2(array):
+#     return sum(array)
 
-def f2(array):
-    return sum(array)
 
-
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -63,8 +62,11 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
-def f4(**args):
-   print('key',args)
+def f4(**kwargs):
+   for key, value in kwargs.items():
+    #    print('key: {}, value: {}'.format(key,value))
+       print(f'key: {key}, value: {value}')
+
 
 # Should print
 # key: a, value: 12
@@ -75,12 +77,12 @@ f4(a=12, b=30)
 # key: city, value: Berkeley
 # key: population, value: 121240
 # key: founded, value: "March 23, 1868"
-# f4(city="Berkeley", population=121240, founded="March 23, 1868")
+f4(city="Berkeley", population=121240, founded="March 23, 1868")
 
-# d = {
-#     "monster": "goblin",
-#     "hp": 3
-# }
+d = {
+    "monster": "goblin",
+    "hp": 3
+}
 
 # # How do you have to modify the f4 call below to make this work?
-# f4(d)
+f4(**d)
--- src/13_file_io.py
+++ src/13_file_io.py
@@ -11,9 +11,22 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+# with open('foo.txt') as foo:
+#     print(foo.read())
+# foo.closed
+# foo.close()
+
+foo = open('foo.txt', 'r')
+read_foo = foo.read()
+print(read_foo)
+foo.close()
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+bar = open('bar.txt', 'w')
+bar.write('type type type\n Testing \nAnother line in')
+bar.close()
